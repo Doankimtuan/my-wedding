b2850d1ee100e79ced71723ad4b77b11a6125791
TuanDK <tuandk@bearacer.vn>
2026-01-29 11:06:05 +0700

    Change og image

--- app/invitation/layout.tsx
+++ app/invitation/layout.tsx
@@ -59,7 +59,7 @@ export const metadata: Metadata = {
     siteName: "Thiệp Cưới Kim Tuấn & Thanh Ngân",
     images: [
       {
-        url: "https://tuan-ngan-wedding.vercel.app/images/og-image.jpg",
+        url: "https://tuan-ngan-wedding.vercel.app/images/og-image.png",
         width: 1200,
         height: 630,
         alt: "Thiệp cưới Kim Tuấn & Thanh Ngân",
